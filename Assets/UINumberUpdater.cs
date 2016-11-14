c0c5f0f9faed871aa567f2c6428acb5017a30fd6
Waevka <waveswallow@gmail.com>
2016-11-14 21:40:32 +0100

    Added updating sliders and descriptions.

---
+++ Assets/UINumberUpdater.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UINumberUpdater : MonoBehaviour {
+
+    string text = "20";
+    public Text sliderValue;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void changeText(float number)
+    {
+        this.text = number.ToString();
+        this.sliderValue.text = text;
+    }
+}
