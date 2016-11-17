0a4e02d11f4da79423cccd48d53e29d3637cb88d
Waevka <waveswallow@gmail.com>
2016-11-18 00:54:33 +0100

    Added working wind force.

--- Assets/Field.cs
+++ Assets/Field.cs
@@ -8,36 +8,81 @@ public class Field : MonoBehaviour {
     GameObject treeRef;
     GameObject fireRef;
 
+    public float fuel;
+    Color color;
+
+    public bool isBurning;
+
+    void Awake() {
+        fuel = 0.5f;
+        color = new Color32(60, 255, 70, 1);
+        isBurning = false; // false
+    }
+
 	// Use this for initialization
 	void Start () {
+        
         treeRef = transform.FindChild("FTree").gameObject;
         fireRef = treeRef.transform.FindChild("FFire").gameObject;
         forestGenerator = GameObject.Find("Forest").gameObject.GetComponent<ForestGenerator>();
         treeRef.GetComponent<Renderer>().material.color = Color.cyan;
+        GetComponent<Renderer>().material.color = color;
+        treeRef.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f) * fuel;
     }
 
     // Update is called once per frame
     void Update () {
+
+        if (fuel < 0.1f)
+        {
+            setIsBurning(false);
+        }
         // update fire parameters (size, strength, etc)
         UpdateFire();
         // update tree parameters (size, color, etc)
         UpdateTree();
         // update field parameters (color)
         UpdateField();
+
     }
 
     void UpdateTree()
     {
-
+        if (isBurning)
+        {
+            treeRef.transform.localScale = new Vector3(0.3f, 0.3f, 0.3f) * fuel;
+        }
     }
 
     void UpdateFire()
     {
-
+        if (isBurning)
+        {
+            fireRef.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f) * fuel;
+        } else  {
+            fireRef.transform.localScale = Vector3.zero;
+        }
     }
 
     void UpdateField()
     {
-
+        if (isBurning)
+        {
+            float g = fuel*100 / 255;
+            color = new Color32(60, 255, 70, 1);
+            color.g = g;
+            GetComponent<Renderer>().material.color = color;
+        }
     }
+
+    public void setIsBurning(bool newIsBurning)
+    {
+        isBurning = newIsBurning;
+    }
+
+    public void setFuel(float newFuel)
+    {
+        fuel = newFuel;
+    }
+
 }
--- Assets/ForestGenerator.cs
+++ Assets/ForestGenerator.cs
@@ -7,12 +7,16 @@ public class ForestGenerator : MonoBehaviour
     GameObject[,] forest;
     public GameObject firePrefab;
     public GameObject fieldPrefab;
+    public WindZone wind;
 
     /// 
     public float size = 20.0f; //TODO: change to int
     public float density;
     public int terrainType;
     public int forestType;
+    public int windDirection;
+    public Vector3 windVector;
+    public float windStrength;
     ///
 
     // Use this for initialization
@@ -22,7 +26,8 @@ public class ForestGenerator : MonoBehaviour
         terrainType = 0;
         forestType = 0;
         size = 20;
-        
+        changeWindDirection(0);
+
         //Generate();
     }
 
@@ -51,6 +56,8 @@ public class ForestGenerator : MonoBehaviour
             {
                 float yPos = ((float)Random.Range(0, 100) / 100) * terrainType;
                 GameObject obj = (GameObject)Instantiate(fieldPrefab, new Vector3(i, yPos, j), Quaternion.identity);
+                obj.GetComponent<Field>().setFuel(Random.Range(0f, 1.0f));
+                obj.GetComponent<Field>().setIsBurning(true);
                 forest[i, j] = obj;
             }
         }
@@ -75,6 +82,41 @@ public class ForestGenerator : MonoBehaviour
     public void changeForestType(int newType)
     {
         forestType = newType;
+    }
+
+    public void changeWindDirection(int newDir)
+    {
+        windDirection = newDir;
+        if (newDir == 0)
+        {
+            wind.transform.localRotation = Quaternion.identity;
+            wind.transform.Rotate(Vector3.left * 90);
+        }
+        if (newDir == 1)
+        {
+            wind.transform.localRotation = Quaternion.identity;
+        }
+        if (newDir == 2)
+        {
+            wind.transform.localRotation = Quaternion.identity;
+            wind.transform.Rotate(Vector3.down*180);
+        }
+        if (newDir == 3)
+        {
+            wind.transform.localRotation = Quaternion.identity;
+            wind.transform.Rotate(Vector3.up * 90);
+        }
+        if (newDir == 4)
+        {
+            wind.transform.localRotation = Quaternion.identity;
+            wind.transform.Rotate(Vector3.down * 90);
+        }
+    }
+
+    public void changeWindStrength(float newStrength)
+    {
+        windStrength = newStrength/4;
+        wind.windMain = windStrength;
     }
 
 }
